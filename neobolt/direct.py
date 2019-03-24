4acc52bf254ef9e84ec00eded04a4c2dfd1201cb
Dean Kooiman <dean.kooiman@cybergrx.com>
2019-03-24 01:56:48 -0600

    bug: Potential deadlock in bolt handshake
    Notes:
     - There is no timeout when waiting to read, this will block a thread until the server responds
     - Added a check using the connection_timeout from config or Default to raise a ServiceUnavailable if a host stops responding
    
    Test Repro:
     - Point your python app at Neo4J running in Docker
     - Docker pause <container>
     - Will deadlock triyng to retrieve a *new* connection

--- neobolt/direct.py
+++ neobolt/direct.py
@@ -874,6 +874,13 @@ def _handshake(s, resolved_address, der_encoded_server_certificate, **config):
     ready_to_read = False
     while not ready_to_read:
         ready_to_read, _, _ = select((s,), (), (), 1)
+
+        timeout = config.get("connection_timeout", DEFAULT_CONNECTION_TIMEOUT)
+        connection_timeout = (perf_counter() - start_timestamp) > timeout
+        if not ready_to_read and connection_timeout:
+            message = "Failed to read any data from server {!r} after connected".format(resolved_address)
+            raise ServiceUnavailable(message)
+
     try:
         data = s.recv(4)
     except (IOError, OSError):  # TODO 2.0: remove IOError alias
